071cb4deca1ec14336000512f071f65e42c80d6b
Augustvilliam <willyp@hotmail.se>
2024-10-23 13:49:22 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import './Components/C1/C1'
+
+function App() {
+  
+  return (
+    <>
+      <C1 />
+      <C2 />
+      <C3 />
+      <C4 />
+      <Testemony />
+      <C5 />
+      <C6 />
+      <C7 />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/C1/C1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const C1 = () => {
+  return (
+    <div>C1
+        <button>Hej</button>
+    </div>
+  )
+}
+
+export default C1
