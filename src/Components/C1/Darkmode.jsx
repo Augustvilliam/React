dec9bbb745940c72d43fb230fb3f39c6ec0d03bd
Augustvilliam <willyp@hotmail.se>
2024-10-25 17:49:02 +0200

    Darkmdoe done

--- src/Components/C1/Darkmode.jsx
+++ src/Components/C1/Darkmode.jsx
@@ -1,20 +1,44 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
-function Darkmode() {
+const Darkmode = () => {
+    const [isDarkMode, setIsDarkMode] = useState(false);
     
+    const toggleDarkMode = () => {
+      const newMode = !isDarkMode;
+      setIsDarkMode(newMode)
+    
+      if (newMode) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+      } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+      }
+    };
+  
+    useEffect(() => {
+      const savedTheme = localStorage.getItem('theme')
+      if (savedTheme=== 'dark'){
+        document.documentElement.setAttribute('data-theme', 'dark');
+        setIsDarkMode(true);
+      } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        setIsDarkMode(false);
+      }
 
-  return (
-
+    }, [])
+  
+    return (
     <div className="tdm">
         <span className="dark-mode-lable">Dark mode</span>
         <label htmlFor="darkmode-switch" className="switch">
-            <input type="checkbox" id="darkmode-switch" onChange="ToggleDarkMode()" />
+            <input checked={isDarkMode} type="checkbox" id="darkmode-switch" onChange={toggleDarkMode} />
             <span className="slider round"></span>
         </label>
         
     </div>
 
-  )
+  );
 }
 
-export default Darkmode
+export default Darkmode;
